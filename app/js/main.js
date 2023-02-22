d87b778ad7124038a4c0d86b7c0513faeb616ede
Atabegovv <atabegov98@mail.ru>
2023-02-22 23:15:14 +0300

    fixed_filter and added_prod

--- app/js/main.js
+++ app/js/main.js
@@ -13,34 +13,6 @@ document.querySelector('.burger-menu').onclick = function(){
   document.querySelector('.menu').classList.toggle('active');
   document.querySelector('.burger-line').classList.toggle('active');
 };
-document.querySelector('.menu').onclick = function(){
-  document.querySelector('.menu').classList.remove('active');
-  document.querySelector('.burger-line').classList.remove('active');
-};
-
-
-
-const selectBtn = document.querySelectorAll('.tab');
-const selectDwn = document.querySelectorAll('.tab_content');
-
-  selectBtn.forEach((tab, index) => {
-    tab.addEventListener('click', ()=> {
-      selectBtn.forEach(tab => {tab.classList.remove('active')});
-      tab.classList.add('active');
-
-      selectDwn.forEach(tab_content => {tab_content.classList.remove('active')});
-      selectDwn[index].classList.add('active');
-    });
-  });
-
-  // const select = document.getElementsByClassName("tab");
-  // for (let i = 0; i < select.length; i++) {
-  //   select[i].addEventListener("click", function() {
-  //     this.classList.remove("active");
-  //   });
-  // };
-
-
 
 
 
